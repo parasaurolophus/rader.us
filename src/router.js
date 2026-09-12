be2504845034b5afa78e8aa9c42a03b695fe0045
Kirk Rader <apps@rader.us>
2026-09-12 05:46:12 -0500

    Use web hash history for navigation for compatibility with dumb hosting services like dreamhost

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 // Copyright (c) Kirk Rader 2026
 
-import { createWebHistory, createRouter } from 'vue-router'
+import { createRouterMatcher, createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
 
@@ -87,6 +87,6 @@ const routes = [
 ]
 
 export const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
 })
